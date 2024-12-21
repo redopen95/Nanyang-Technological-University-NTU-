80d8789e991b93d99f98a59c54658e8e47881024
Redopen95 <90855397+redopen95@users.noreply.github.com>
2024-12-21 17:16:36 +0800

    Delete ALGORITHMS & COMPUTING/Lab & Tutorials directory

--- ALGORITHMS & COMPUTING/Lab & Tutorials/My Solutions/U1740947L_Lab7_Density.py
+++
@@ -1,9 +0,0 @@
-#Question 6 (Script File)
-
-from U1740947L_Lab7_Qn6_FunctionFile import countryDensity                      #Importing function file
-
-Country=input('Please enter a country name: ')                                  #Prompts user to enter a country name                            
-Area=float(input('Please enter the total land area of the country in km2: '))   #Prompts user to enter land area of country
-Population=float(input('Please enter the total population of the country: '))   #Prompts user to enter population of country
-                                                                                #Print output
-print('The population density of {:s} is {:.0f} people per km2'.format(Country,countryDensity(Country,Area,Population)))
--- ALGORITHMS & COMPUTING/Lab & Tutorials/My Solutions/U1740947L_Lab7_Qn6_FunctionFile.py
+++
@@ -1,8 +0,0 @@
-#Question 6 (Function File)
-
-def countryDensity(Country,Area,Population):                                    #Defining function for calculating country density
-    """This function calculates the population density of a given country
-    Format of call: countryDensity(Country,Area,Population)
-    Returns the population density of a given country"""
-    return Population/Area                                                      #Returns the value of population density
-
